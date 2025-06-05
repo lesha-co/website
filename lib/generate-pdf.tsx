1bd3fb30fe7b26ace0f2ba6fffbdf1c5cf4e97b6
Aleksei Kuzmichev <cordial.job2650@lesha.co>
2025-06-05 17:33:48 +0200

    added some skills

--- lib/generate-pdf.tsx
+++ lib/generate-pdf.tsx
@@ -4,33 +4,34 @@ import { CVPdf } from "../app/ui-cv/CVPdf";
 import * as PDF from "@react-pdf/renderer";
 import { join } from "node:path";
 
+// I only need to register these fonts once as PDF.Font is effectively a singleton
+PDF.Font.register({
+  family: "Geist",
+  fonts: [
+    {
+      src: "./public/geist/Geist-Medium.ttf",
+      fontStyle: "normal",
+      fontWeight: "medium",
+    },
+    {
+      src: "./public/geist/Geist-Regular.ttf",
+      fontStyle: "normal",
+      fontWeight: "normal",
+    },
+    {
+      src: "./public/geist/Geist-Bold.ttf",
+      fontStyle: "normal",
+      fontWeight: "bold",
+    },
+  ],
+});
+
 export async function generatePdf(
   cv: LocalizedObject<CV>,
 
   photo: boolean,
 ) {
   const photoPath = join(process.cwd(), "public", cv.personal.photoPdf);
-
-  PDF.Font.register({
-    family: "Geist",
-    fonts: [
-      {
-        src: "./public/geist/Geist-Medium.ttf",
-        fontStyle: "normal",
-        fontWeight: "medium",
-      },
-      {
-        src: "./public/geist/Geist-Regular.ttf",
-        fontStyle: "normal",
-        fontWeight: "normal",
-      },
-      {
-        src: "./public/geist/Geist-Bold.ttf",
-        fontStyle: "normal",
-        fontWeight: "bold",
-      },
-    ],
-  });
 
   const buf = await renderToBuffer(
     <CVPdf photo={photo ? photoPath : null} cv={cv} />,
